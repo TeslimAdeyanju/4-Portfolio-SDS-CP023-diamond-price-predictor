846bc1ab81cd9b51dd1b58dff2cbe8c51470b768
Reema Raghava <reema14a@gmail.com>
2025-03-25 06:17:47 +0530

    Fixed issues with model and API

--- submissions-team/reema-raghava/api.py
+++ submissions-team/reema-raghava/api.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 model = joblib.load('model/PricePredictor.pkl')
-sc = joblib.load('model/StdScaler.pkl')
 oe = joblib.load('model/OrdEncoder.pkl')
 
 app = FastAPI(
@@ -26,52 +25,55 @@ class DiamondData(BaseModel):
     y: float
     z: float
 
-    @app.post(path = '/api/predict',
+@app.post(path = '/api/predict',
               summary = 'Predict the price of a diamond',
               description = 'Predicting the price of a diamond based on its various attributes using XGBoost',
               tags = ['Diamond Price Predictor']
         )
-    def predict(input_data: DiamondData):
-        """
-            :param diamond_data: 
-                carat - 
-                cut - 
-                color - 
-                clarity - 
-                depth - 
-                table - 
-                x - 
-                y - 
-                z - 
-            :return:
-                Predicted price of the diamond
-            
-        """
-
-        # Convert the input data to a numpy array
-        input_data = np.array([
-                input_data.carat,
-                input_data.cut,
-                input_data.color,
-                input_data.clarity,
-                input_data.depth,
-                input_data.table,
-                input_data.x,
-                input_data.y,
-                input_data.z
-            ]).reshape(1, -1)
+def predict(input_data: DiamondData):
+    """
+        :param diamond_data: 
+            carat - 
+            cut - 
+            color - 
+            clarity - 
+            depth - 
+            table - 
+            x - 
+            y - 
+            z - 
+        :return:
+            Predicted price of the diamond
         
-        # Encode the categorical data
-        encoded_data = oe.transform(input_data[:, 1:4])
-        input_data[:, 1:4] = encoded_data
+    """
+    # Convert input data to dictionary
+    input_dict = input_data.dict()
 
-        # Scale the numerical data
-        scaled_data = sc.transform(input_data)
+    # Convert the input data to a numpy array
+    raw_data = np.array([
+            
+            input_dict["cut"],
+            input_dict["color"],
+            input_dict["clarity"],
+            input_dict["carat"],
+            input_dict["depth"],
+            input_dict["table"],
+            input_dict["x"],
+            input_dict["z"],
+            input_dict["y"]
+        ]).reshape(1, -1)
+    
+    # Encode the categorical data
+    encoded_data = oe.transform(raw_data[:, 0:3])
+    raw_data[:, 0:3] = encoded_data
 
-        # Make the prediction
-        prediction = model.predict(scaled_data)[0]
+    # Convert entire array to float for XGBoost compatibility
+    raw_data = raw_data.astype(float)
 
-        return {'prediction': prediction}
+    # Make the prediction
+    prediction = model.predict(raw_data)
+    
+    return {"prediction": float(prediction[0])}
     
 
 
