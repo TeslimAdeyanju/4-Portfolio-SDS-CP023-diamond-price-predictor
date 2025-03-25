01eb65c2ffa73a6a11d8d4713bb4d81b7375fc46
Reema Raghava <reema14a@gmail.com>
2025-03-25 06:52:50 +0530

    Removed port

--- submissions-team/reema-raghava/api.py
+++ submissions-team/reema-raghava/api.py
@@ -81,7 +81,3 @@ def predict(input_data: DiamondData):
     return {"prediction": float(prediction[0])}
     
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Get PORT from environment, default to 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
